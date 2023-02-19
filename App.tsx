0778ad04de33267ab5959e357f03e46ccdfa455b
d80082 <rory.robinson@d-fine.co.uk>
2023-02-19 19:14:17 +0000

    Beginning the navigation implementation

--- App.tsx
+++ App.tsx
@@ -1,18 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Home from './Home';
+
+const Stack = createStackNavigator();
 
 function App(): JSX.Element {
   return (
     <>
       <StatusBar barStyle="dark-content" hidden />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerMode: 'screen',
+          }}>
+          <Stack.Screen name="Home" options={{headerShown: false}}>
+            {props => <Home {...props} username="sports fan" />}
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
       <Home username="sports fan" />
     </>
   );
